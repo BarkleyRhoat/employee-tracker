4a4955a0ea6a2d4d035a20b23fc91961a31ba2a1
BarkleyRhoat <barkleyrhoat@gmail.com>
2023-12-26 17:28:28 -0500

    added index prompts

--- db/schema.sql
+++ db/schema.sql
@@ -3,13 +3,13 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE department(
-department_id INT AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR(30)
+CREATE TABLE departments(
+id INT PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(30) NOT NULL 
 );
 
 CREATE TABLE role(
-role_id INT PRIMARY KEY AUTO_INCREMENT
+id INT PRIMARY KEY AUTO_INCREMENT
 title VARCHAR(30),
 salary DECIMAL,
 department_id INT.
@@ -17,7 +17,7 @@ FOREIGN KEY (department_id) REFERENCES department(department_id)
 );
 
 CREATE TABLE employee(
-employee_id INT PRIMARY KEY AUTO_INCREMENT,
+id INT PRIMARY KEY AUTO_INCREMENT,
 first_name VARCHAR(30),
 last_name VARCHAR(30),
 role_id INT,
--- main/index.js
+++ main/index.js
@@ -1,39 +1,95 @@
 const { table } = require('table');
-
+const inquirer = require('inquirer');
 const mysql = require('mysql2/promise');
 
 let db = null;
 
 const init = async () => {
-    db = await mysql.createConnection(
-        {
-            host: 'localhost',
-            // MySQL username, 
-            user: 'root', 
-            password: 'root',
-            database: 'employee_db'
-        }
-    );
+    db = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'employee_db'
+    });
+
     console.log(`Connected to the employee_db database.`);
-    console.log(db);
-    
-    // TODO use inquirer if you want
 
-  // use prepared_statement
-  // const objInput = {
-  //   // must match database column and values
-  //   name: "Nelson"
-  // }
-  // const idata = await db.query("INSERT INTO island SET ?", objInput)
+    // Use inquirer prompts
+    const action = await inquirer.prompt({
+        type: 'list',
+        name: 'action',
+        message: 'What would you like to do?',
+        choices: [
+            'View All Employees',
+            'Add Employee',
+            'Update Employee Role',
+            'View All Roles',
+            'Add Role',
+            'View All Departments',
+            'Add Department',
+            'Quit'
 
-  const results = await db.query("SELECT * FROM department;");
+        ],
+    });
 
-  // get data from results
-  const data = results[0];
-  console.log(data);
+    switch (action.action) {
+        case 'View all departments':
+            await viewAllDepartments();
+            break;
+        case 'View all employees':
+            await viewAllEmployees();
+            break;
+        case 'Add a department':
+            await addDepartment();
+            break;
+        // Add cases for other choices
+    }
 
-  const arrOfArr = data.map( row => Object.values(row));
-  //print table
-  console.log(table(arrOfArr));
-}
+    db.end(); // Close the database connection when done
+};
+
+const viewAllDepartments = async () => {
+    const results = await db.query('SELECT * FROM department;');
+    const data = results[0];
+    const arrOfArr = data.map(row => Object.values(row));
+    console.log(table(arrOfArr));
+};
+
+const viewAllEmployees = async () => {
+    // Implement logic for viewing all employees
+};
+
+const addDepartment = async () => {
+    const departmentName = await inquirer.prompt({
+        type: 'input',
+        name: 'name',
+        message: 'Enter the name of the department:',
+    });
+
+    // Implement logic to add a department to the database using departmentName.name
+};
+
 init();
+
+ 
+    
+//     // TODO use inquirer if you want
+
+//   // use prepared_statement
+//   // const objInput = {
+//   //   // must match database column and values
+//   //   name: "Nelson"
+//   // }
+//   // const idata = await db.query("INSERT INTO island SET ?", objInput)
+
+//   const results = await db.query("SELECT * FROM department;");
+
+//   // get data from results
+//   const data = results[0];
+//   console.log(data);
+
+//   const arrOfArr = data.map( row => Object.values(row));
+//   //print table
+//   console.log(table(arrOfArr));
+// }
+// init();
