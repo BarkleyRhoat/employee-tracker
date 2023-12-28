b8dfaa8aecabcbcfcab0a3effc1ba083ef9cb395
BarkleyRhoat <barkleyrhoat@gmail.com>
2023-12-27 19:54:35 -0500

    autoincrement on schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,15 +4,15 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-    department_id INT PRIMARY KEY,
+    department_id INT PRIMARY KEY AUTO_INCREMENT,
     department_name VARCHAR(50) NOT NULL 
 );
 
 CREATE TABLE role (
-    role_id INT PRIMARY KEY,
+    role_id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT,
+    department_id INT AUTO_INCREMENT,
     FOREIGN KEY (department_id) REFERENCES department(department_id)
 );
 
@@ -20,9 +20,9 @@ CREATE TABLE employee (
     employee_id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT,
+    role_id INT AUTO_INCREMENT,
     manager_id INT,
-    department_id INT, 
+    department_id INT AUTO_INCREMENT, 
     FOREIGN KEY (role_id) REFERENCES role(role_id),
     FOREIGN KEY (manager_id) REFERENCES employee(employee_id),
     FOREIGN KEY (department_id) REFERENCES department(department_id) 
